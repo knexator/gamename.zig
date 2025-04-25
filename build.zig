11a5c217bdbc191aa4b35b04a8e0a4bacbfa8630
knexator <daniel.hurtadoramirez314@gmail.com>
2025-04-25 18:29:24 +0200

    reorder build.zig

--- build.zig
+++ build.zig
@@ -4,53 +4,102 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const hot_reloading = b.option(
+    const hot_reloadable = b.option(
         bool,
         "hot-reloadable",
         "If true, will compile the game logic as a separate dynamic library.",
     ) orelse (optimize == .Debug);
 
-    const sdl_lib = b.dependency("sdl", .{
-        .target = target,
-        .optimize = optimize,
-    }).artifact("SDL3");
+    const emit_llvm_ir = b.option(bool, "emit-llvm-ir", "Emit LLVM IR") orelse false;
 
+    const install_step = b.getInstallStep();
+    const run_step = b.step("run", "Run the app");
+    const test_step = b.step("test", "Run unit tests");
+    const check_step = b.step("check", "Check if the project compiles");
+
+    // TODO: delete this variable
     const kommon_module = b.dependency("kommon", .{
         .target = target,
         .optimize = optimize,
     }).module("kommon");
 
+    build_for_desktop(b, .{
+        .install = install_step,
+        .run = run_step,
+        .unit_test = test_step,
+        .check = check_step,
+    }, .{
+        .target = target,
+        .optimize = optimize,
+        .hot_reloadable = hot_reloadable,
+        .emit_llvm_ir = emit_llvm_ir,
+    }, kommon_module);
+}
+
+fn build_for_desktop(
+    b: *std.Build,
+    steps: struct {
+        run: *std.Build.Step,
+        install: *std.Build.Step,
+        check: *std.Build.Step,
+        unit_test: *std.Build.Step,
+    },
+    options: struct {
+        target: std.Build.ResolvedTarget,
+        optimize: std.builtin.OptimizeMode,
+        hot_reloadable: bool,
+        emit_llvm_ir: bool,
+    },
+    kommon_module: *std.Build.Module,
+) void {
+    const sdl_lib = b.dependency("sdl", .{
+        .target = options.target,
+        .optimize = options.optimize,
+    }).artifact("SDL3");
+
     const build_options = b.addOptions();
-    if (hot_reloading) {
+    if (options.hot_reloadable) {
         const game_module = b.createModule(.{
             .root_source_file = b.path("src/game.zig"),
-            .target = target,
-            .optimize = optimize,
+            .target = options.target,
+            .optimize = options.optimize,
             .pic = true,
         });
         const game_lib = b.addLibrary(.{
             .name = "game",
             .root_module = game_module,
             .linkage = .dynamic,
-            // waiting for https://github.com/ziglang/zig/issues/23442
+            // TODO(zig): uncomment this line after solving https://github.com/ziglang/zig/issues/23442
             // .use_llvm = false,
         });
-        b.installArtifact(game_lib);
         const install_game_lib = b.addInstallArtifact(game_lib, .{});
-        b.getInstallStep().dependOn(&install_game_lib.step);
+        steps.install.dependOn(&install_game_lib.step);
 
-        // HACK
-        // hot_reload_options.args.clearRetainingCapacity();
-        // build_options.addOptionPath("lib_path", install_game_lib.emitted_bin.?);
-        const path_to_lib = b.fmt("{s}/{s}/{s}", .{
-            b.install_path,
-            switch (install_game_lib.dest_dir.?) {
-                .lib => "lib",
-                .bin => "bin",
-                .custom, .header, .prefix => unreachable,
-            },
-            install_game_lib.dest_sub_path,
-        });
+        if (options.emit_llvm_ir) {
+            steps.install.dependOn(&b.addInstallFile(
+                game_lib.getEmittedLlvmIr(),
+                "game.ir",
+            ).step);
+        }
+
+        {
+            const game_unit_tests = b.addTest(.{
+                .root_module = game_module,
+            });
+            const run_game_unit_tests = b.addRunArtifact(game_unit_tests);
+            steps.unit_test.dependOn(&run_game_unit_tests.step);
+        }
+        {
+            // TODO(zig): delete game_check after solving https://github.com/ziglang/zig/issues/18877
+            const game_check = b.addLibrary(.{
+                .name = "game",
+                .root_module = game_module,
+                .linkage = .dynamic,
+            });
+            steps.check.dependOn(&game_check.step);
+        }
+
+        const path_to_lib = b.getInstallPath(install_game_lib.dest_dir.?, install_game_lib.dest_sub_path);
         build_options.addOption(?[]const u8, "game_dynlib_path", path_to_lib);
     } else {
         build_options.addOption(?[]const u8, "game_dynlib_path", null);
@@ -58,8 +107,8 @@ pub fn build(b: *std.Build) void {
 
     const exe_module = b.createModule(.{
         .root_source_file = b.path("src/sdl_platform.zig"),
-        .target = target,
-        .optimize = optimize,
+        .target = options.target,
+        .optimize = options.optimize,
     });
     exe_module.linkLibrary(sdl_lib);
     exe_module.addImport("kommon", kommon_module);
@@ -69,10 +118,13 @@ pub fn build(b: *std.Build) void {
         .name = "game",
         .root_module = exe_module,
     });
-    b.installArtifact(exe);
+    steps.install.dependOn(&b.addInstallArtifact(exe, .{}).step);
 
-    if (b.option(bool, "emit-llvm-ir", "Emit LLVM IR") orelse false) {
-        b.getInstallStep().dependOn(&b.addInstallFile(exe.getEmittedLlvmIr(), "platform.ir").step);
+    if (options.emit_llvm_ir) {
+        steps.install.dependOn(&b.addInstallFile(
+            exe.getEmittedLlvmIr(),
+            "sdl_platform.ir",
+        ).step);
     }
 
     {
@@ -82,8 +134,7 @@ pub fn build(b: *std.Build) void {
             run_cmd.addArgs(args);
         }
 
-        const run_step = b.step("run", "Run the app");
-        run_step.dependOn(&run_cmd.step);
+        steps.run.dependOn(&run_cmd.step);
     }
 
     {
@@ -91,30 +142,49 @@ pub fn build(b: *std.Build) void {
             .root_module = exe_module,
         });
         const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-        const test_step = b.step("test", "Run unit tests");
-        test_step.dependOn(&run_exe_unit_tests.step);
+        steps.unit_test.dependOn(&run_exe_unit_tests.step);
     }
 
     {
+        // TODO(zig): delete exe_check after solving https://github.com/ziglang/zig/issues/18877
         const exe_check = b.addExecutable(.{
-            .name = "foo",
+            .name = "sdl_platform",
             .root_module = exe_module,
         });
-
-        const check = b.step("check", "Check if the project compiles");
-        check.dependOn(&exe_check.step);
+        steps.check.dependOn(&exe_check.step);
     }
+}
 
+fn build_for_web(
+    b: *std.Build,
+    steps: struct {
+        run: *std.Build.Step,
+        install: *std.Build.Step,
+        check: *std.Build.Step,
+        unit_test: *std.Build.Step,
+    },
+    options: struct {
+        target: std.Build.ResolvedTarget,
+        optimize: std.builtin.OptimizeMode,
+        hot_reloadable: bool,
+        emit_llvm_ir: bool,
+    },
+    kommon_module: *std.Build.Module,
+) void {
+    _ = steps;
     // TODO: wasm
     if (false) {
+        // const target = b.resolveTargetQuery(.{
+        //     .cpu_arch = .wasm32,
+        //     .os_tag = .freestanding,
+        // });
         const wasm_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = b.resolveTargetQuery(.{
                 .cpu_arch = .wasm32,
                 .os_tag = .freestanding,
             }),
-            .optimize = optimize,
+            .optimize = options.optimize,
         });
         wasm_module.addImport("kommon", kommon_module);
 
